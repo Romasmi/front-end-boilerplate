ef06347d6f441de1c74ce69d9c7a2f2298a435fc
romasmi <romasmi@ro.ru>
2022-03-02 16:59:47 +0300

    Add SVG sprites

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
-const spritesmith = require('gulp.spritesmith');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 const sourcemaps = require('gulp-sourcemaps');
@@ -8,46 +7,30 @@ const del = require('del');
 const svgSprite = require('gulp-svg-sprite');
 
 let path = {
-    'sprite': 'assets/template/image/sprite/*',
+    'sprite': 'assets/template/image/sprite/',
     'image': 'assets/template/image/',
     'css': 'assets/template/style/css/',
-    'scss': 'assets/template/style/scss/*',
+    'scss': 'assets/template/style/scss/',
     'html': '*.html',
     'js': 'assets/template/js/main.js',
     'template': 'assets/template/',
 };
 
-function createSpritePNG() {
-    del.sync(['assets/template/image/sprite*.png']);
-    const spriteSheetNamePostfix = Date.now();
-    let spriteData = gulp.src(path.sprite + '.png').pipe(spritesmith({
-        imgName: `sprite${spriteSheetNamePostfix}.png`,
-        cssName: 'sprite.css',
-        imgPath: `../../image/sprite${spriteSheetNamePostfix}.png`,
-        cssOpts: {
-            cssSelector: function(sprite) {
-                return '.icon-' + sprite.name.replace(/_/g, '-');
-            }
-        }
-    }));
-
-    spriteData.img.pipe(gulp.dest(path.image));
-    return spriteData.css.pipe(gulp.dest(path.css));
-}
-
-function createSpriteSVG() {
-    return gulp.src(path.sprite + '.svg')
+function sprite() {
+    del.sync([path.image + 'sprite*.svg']);
+    return gulp.src(path.sprite + '*.svg')
         .pipe(svgSprite({
             mode: {
-                symbol: {
-                    dest: 'image',
-                    sprite: 'sprite.svg'
-                },
                 css: {
+                    dest: '/' + path.scss,
                     render: {
-                        css: true
-                    }
-                }
+                        scss: true
+                    },
+                    dimensions: true,
+                    prefix: '.icon-%s',
+                    sprite: '/' + path.image + 'sprite.svg',
+                    common: 'icon'
+                },
             },
             shape: {
                 id: {
@@ -63,22 +46,20 @@ function createSpriteSVG() {
 }
 
 function style() {
-    return gulp.src(path.scss)
+    return gulp.src(path.scss + '*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'expanded'
         }))
-        .pipe(sourcemaps.write())
         .pipe(autoprefixer([
             'Android >= 2.2',
             'Chrome >= 3.5',
             'Firefox >= 3.5',
-            'Explorer >= 8',
-            'iOS >= 4.3',
+            'iOS >= 8',
             'Opera >= 10.1',
             'Safari >= 3',
-            'IE >= 9'
         ]))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(path.css))
         .pipe(browserSync.stream());
 }
@@ -90,10 +71,19 @@ function watch() {
         }
     });
 
-    gulp.watch(path.sprite + '.png', createSpritePNG);
-    gulp.watch(path.sprite + '.svg', createSpriteSVG);
-    gulp.watch(path.scss, style);
+    gulp.watch(path.sprite + '*.svg', sprite);
+    gulp.watch(path.scss + '*.scss', style);
     gulp.watch([path.html, path.js]).on('change', browserSync.reload);
 }
 
+function build()
+{
+    sprite();
+    style();
+}
+
 exports.default = watch;
+exports.svg = sprite;
+exports.style = style;
+
+exports.build = build;
